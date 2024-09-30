9882ba39a3eb60832f830ec29958bce0cdb5b94f
enkopavle <enkopavle@gmail.com>
2024-09-30 18:16:14 +0300

    main: added redux provider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
+import {Provider as ReduxProvider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import {Navigation} from '@/navigation';
+import store, {persistor} from '@/store';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Navigation />
-    </>
+    <ReduxProvider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Navigation />
+      </PersistGate>
+    </ReduxProvider>
   );
 }
 
